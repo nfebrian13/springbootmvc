b22ebd698688c4daa8b18aece764f17d6990c074
nfebrian13 <febrian.nana@gmail.com>
2020-03-24 13:14:43 +0700

    commit

---
+++ springbootmvc/src/main/java/com/nana/springbootmvc/controllers/IndexController.java
@@ -0,0 +1,5 @@
+package com.nana.springbootmvc.controllers;
+
+public class IndexController {
+
+}
---
+++ springbootmvc/src/main/java/com/nana/springbootmvc/services/IndexService.java
@@ -0,0 +1,13 @@
+package com.nana.springbootmvc.services;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexService {
+
+	@RequestMapping("/")
+	public String index() {
+		return "index";
+	}
+}
